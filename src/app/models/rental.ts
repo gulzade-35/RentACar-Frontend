8555c985b265914323824edf156499a9b94d8cdb
gülzade odabaş <gulzadeodabas27@gmail.com>
2021-04-04 01:34:46 +0300

    17. gün

---
+++ src/app/models/rental.ts
@@ -0,0 +1,8 @@
+export interface Rental{
+    rentalId:number;
+    brandName:number;
+    customerFirstName:string;
+    customerLastName:string;
+    rentDate:Date;
+    returnDate:Date;
+}
---
+++ src/app/services/car.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Car } from '../models/car';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  apiUrl = "https://localhost:44326/api/cars/getall";
+
+  constructor(private httpClient:HttpClient) { }
+
+  getCars():Observable<ListResponseModel<Car>>{
+    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl)
+  }
+
+}
